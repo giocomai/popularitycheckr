31cf17c5fe0fcd013d91c6edb3dc25eadabe1422
Giorgio Comai <g@giorgiocomai.eu>
2020-04-28 14:28:22 +0200

    export draft pc_process_backlinks

--- R/pc_process_backlinks.R
+++ R/pc_process_backlinks.R
@@ -1,12 +1,31 @@
-pc_process_backlinks <- function(backlinks) {
+#' Process backlinks
+#'
+#' Uploads backliks to relevant google drive spreadsheet, consdering custom blacklist and whitelists.
+#'
+#' @param backlinks A data frame with backlinks, typically generated with `pc_get_backlinks()` or retrieved with `pc_downlodad_backlinks()`
+#' @param include_domain Logical, defaults to TRUE. Include the base domain in the `landingPagePath` column for clarity, assuming https protocol.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' pc_process_backlinks(pc_get_backlinks())
+#' }
+pc_process_backlinks <- function(backlinks, include_domain = TRUE) {
   df <- backlinks %>%
     dplyr::group_by(source, fullReferrer, landingPagePath) %>%
     dplyr::tally() %>%
     dplyr::arrange(dplyr::desc(n)) %>%
     dplyr::mutate(blacklist_source = "",
-                  remove_this = "",
+                  remove_referrer = "",
                   whitelist_source = "",
-                  keep_this = "")
+                  keep_referrer = "")
+
+  if (include_domain == TRUE) {
+    df <- df %>%
+      dplyr::mutate(landingPagePath = paste0("https://", pc_set_domain(), landingPagePath))
+  }
 
   current_spreadsheet <- pc_find_dribble(type = "inputs", content = "process")
 
@@ -27,5 +46,19 @@ pc_process_backlinks <- function(backlinks) {
                                   sheet = "process")
     }
   }
+
+}
+
+pc_process_wb <- function() {
+  current_spreadsheet <- pc_find_dribble(type = "inputs", content = "process")
+
+  current_sheet_names <- googlesheets4::sheet_names(current_spreadsheet)
+
+  if (is.element(el = whitelist_source, set = current_sheet_names)) {
+    whitelist_source_df <- googlesheets4::read_sheet(ss = current_spreadsheet, sheet = "whitelist_source")
+  } else {
+
+  }
+
 
 }
